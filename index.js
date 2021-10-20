593cb14bfe25c0dd933056969ac27fe236ac193b
Joe <jjprevite@gmail.com>
2021-10-20 09:16:20 +0000

    wip

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ *
+ * @param str path to value you want to lookup
+ * @returns function to get value at path
+ * @example
+ * import dotpath from "dotpath"
+ * const lookupName = dotpath("name")
+ * const name = lookupName({name: "Joe"})
+ * console.log(name) // "Joe"
+ */
 function dotpath(str) {
-    var parts = str.toString().split(".");
+    var parts = str.split(".");
     var len = parts.length;
     return function parse(obj) {
+        // Could be a key in an object
+        // or an index in an array
         var testKey;
+        var result;
         for (var i = 0; i < len; ++i) {
             testKey = parts[i];
-            if (!obj)
-                return;
-            obj = obj[testKey];
+            if (!obj) {
+                result = undefined;
+                break;
+            }
+            result = obj[testKey];
         }
-        return obj;
+        return result;
     };
 }
 exports.default = dotpath;
--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
-function dotpath(str: any) {
-  const parts = str.toString().split(".");
+/**
+ *
+ * @param str path to value you want to lookup
+ * @returns function to get value at path
+ * @example
+ * import dotpath from "dotpath"
+ * const lookupName = dotpath("name")
+ * const name = lookupName({name: "Joe"})
+ * console.log(name) // "Joe"
+ */
+function dotpath(str: string) {
+  const parts = str.split(".");
   const len = parts.length;
 
-  return function parse(obj: any) {
-    let testKey;
+  return function parse<KeyValueType>(obj: {
+    [key: string]: KeyValueType;
+  }): any {
+    // Could be a key in an object
+    // or an index in an array
+    let testKey: string | number;
 
-    for (var i = 0; i < len; ++i) {
+    for (let i = 0; i < len; ++i) {
       testKey = parts[i];
 
-      if (!obj) return;
+      if (!obj) return undefined;
 
       obj = obj[testKey];
     }
